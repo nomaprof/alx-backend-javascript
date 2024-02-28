5d9aa4e2ed3d315d037a14b928996fcbe1012bb6
nomaprof <nomaprof@gmail.com>
2024-02-28 14:24:14 +0000

    testing software

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,10 @@
+// Calculation function.
+
+function calculateNumber(a, b) {
+    firstInt = Math.round(a);
+    secondInt = Math.round(b);
+    return firstInt + secondInt;
+}
+
+module.exports = calculateNumber;
+
